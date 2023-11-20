654b7ee337dfe8d9e4e87156778dd26886a9e73b
RodrigoBG1 <129990916+RodrigoBG1@users.noreply.github.com>
2023-11-20 10:38:13 -0600

    Update ProyectoFinalJuego.py

--- ProyectoFinalJuego.py
+++ ProyectoFinalJuego.py
@@ -1,1 +1,138 @@
+import os
+import time
+Titulo = """
+$$$$$$$\                      $$\                        $$\     $$\                         $$\       
+$$  __$$\                     $$ |                       $$ |    $$ |                        $$ |      
+$$ |  $$ | $$$$$$\  $$$$$$$\  $$ |  $$\        $$$$$$\ $$$$$$\ $$$$$$\    $$$$$$\   $$$$$$$\ $$ |  $$\ 
+$$$$$$$\ | \____$$\ $$  __$$\ $$ | $$  |       \____$$  _$$  _|\_$$  _|   \____$$\ $$  _____|$$ | $$  |
+$$  __$$\  $$$$$$$ |$$ |  $$ |$$$$$$  /        $$$$$$$ | $$ |    $$ |     $$$$$$$ |$$ /      $$$$$$  / 
+$$ |  $$ |$$  __$$ |$$ |  $$ |$$  _$$<        $$  __$$ | $$ |$$\ $$ |$$\ $$  __$$ |$$ |      $$  _$$<  
+$$$$$$$  |\$$$$$$$ |$$ |  $$ |$$ | \$$\       \$$$$$$$ | \$$$$  |\$$$$  |\$$$$$$$ |\$$$$$$$\ $$ | \$$\ 
+\_______/  \_______|\__|  \__|\__|  \__|       \_______|  \____/  \____/  \_______| \_______|\__|  \__|
+"""
+Personaje = """
+                        .------.
+                       / j      ;
+                      :.d;       ;
+                      $$P        :
+           .m._       $$         :
+          dSMMSSSss.__$$b.    __ :
+         :MMSMMSSSMMMSS$$$b  $$P ;
+         SMMMSMMSMMMSSS$$$$     :b
+        dSMMMSMMMMMMSSMM$$$b.dP SSb.
+       dSMMMMMMMMMMSSMMPT$$=-. /TSSSS.
+      :SMMMSMMMMMMMSMMP  `$b_.'  MMMMSS.
+      SMMMMMSMMMMMMMMM \  .'\    :SMMMSSS.             
+     dSMSSMMMSMMMMMMMM  \/\_/; .'SSMMMMSSSm            
+    dSMMMMSMMSMMMMMMMM    :.;'" :SSMMMMSSMM;          
+  .MMSSSSSMSSMMMMMMMM;    :.;   MMSMMMMSMMM;           
+ dMSSMMSSSSSSSMMMMMMM;    ;.;   MMMMMMMSMMM            
+:MMMSSSSMMMSSP^TMMMMM     ;.;   MMMMMMMMMMM            
+MMMSMMMMSSSSP   `MMMM     ;.;   :MMMMMMMMM;
+"TMMMMMMMMMM      TM;    :`.:    MMMMMMMMM;
+   )MMMMMMM;     _/     :`.:    :MMMMMMMM
+  d$SS$$$MMMb.  |._\    :`.:     MMMMMMMM
+  T$$S$$$$$$$$$$m;O"-;`.:_.-  MMMMMMM;
+ :$$$$$$$$$$$$$$$b_l./ ;`.:    mMMSSMMM;
+ :$$$$$$$$$$$$$$$$$$$./;`.:  .$$MSMMMMMM
+  $$$$$$$$$$$$$$$$$$$$. `.:.$$$$SMSSSMMM;
+  $$$$$$$$$$$$$$$$$$$$$. .:$$$$$SSMMMMMMM
+  :$$$$$$$$$$$$$$$$$$$$$.//.:$$$$SSSSSSSMM;
+  :$$$$$$$$$$$$$$$$$$$$$$.`.:$$SSSSSSSMMMP
+"""
 
+print(Titulo, Personaje)
+
+dialogos = """
+                    dS$$S$S$S$S$S$S$$Sb                    
+                   :$$S^S$S$S$S$S$S^S$$;                   
+                   SSP   `^$S$S$^'   TSS                   
+                   $$       `"'       $$                   
+                  _SS ,-           -  SS_      ___________________________________            
+                 :-.|  _    - .-   _  |.-;    |                                   |
+                 :\(; ' "-._.'._.-" ` |)/;    |                                   |
+                  \`|  , o       o .  |'/     |                                   |
+                   ":     -'   `-     ;"      |                                   |
+                     ;.              :        |                                   |
+                     : `    ._.    ' ;       /____________________________________|            
+                   .sSb   ._____.   dSs.                   
+                _.d8dSSb.   ._.   .SSbT8b._                
+            _.oOPd88SSSS T.     .P SSSS888OOo.             
+        _.mMMOOPd888SSSSb TSqqqSP dSSSS88OMOOOMm._         
+     .oOMMMOMOOM8O8OSSSSSb TSSSP dSSSSS8OOMMOOMMOOOo._     
+   .OOMMOOOMMOOMOOOO  "^SSSTSSP dSSS^"OOOOMMOOMMMOOMMMb.   
+  dOOOMMMOMMOOOMOOOO      "^SSSS^"   :OOO8MMMOOMMOOMMOMMb  
+ :MMMOOMMOMMOOMMO8OS         `P      8O8OPdMMOOMMOMMOMMMM; 
+ MMMMOOMMMMMOOMbTO8S;               :8888MMMMMOMMOMMOMMMMM 
+ OMMMMOOMMMMOOOMMOOOS        S     :O8OPdMOMMMOMOMMOOMMMMO 
+:OMMMMOOMMOMMOOMbTObTb.     :S;   .PdOPdMOOMMMMMOMMOMMMMMO;
+MOOMMMMOMMOMMOOMMMOObTSSg._.SSS._.PdOPdMOOMMMMOMMMMOMMMMOOM
+MMOMMMMOMMMOMMOOMMbT8bTSSSSSSSSSPd8OPdOOOMMMMOOMMMMOMMMOOMM
+MMOMMMOMMMMMOMMOOMMMbT8bTSSSSSPd88PdOOOOMMMMOOMMMMMMMMOOMMM """
+
+class Juego:
+    def __init__(self, nom):
+        self.nom = nom
+        self.decisiones = {
+            "inicio": {
+                "mensaje": "Buenas tardes {} hemos estado viendo el banco central de Mexico y el de Buenos Aires, \ncreemos que tu eres la mejor opcion para esete atraco. De que mision quieres formar parte?".format(self.nom),
+                "opciones": {
+                    "mexico": "Mexico",
+                    "buenos aires": "Buenos Aires"
+                }
+            },
+    "Mexico": {
+        "mensaje": "Bienvenido a la mision de Mexico, en esta mision asaltaremos al banco central de Mexico. Para este tenemos opciones de atraco: \n\t- Cabar un tunel \n\t- Asalto a mano amrada",
+        "opciones": {
+            "tunel": "tunel",
+            "mano": "mano"
+        }
+            },
+        "tunel": {
+            "mensaje": "Has elegido cabar un tunel para poder hacer el atraco, ",
+            "opciones": {
+                "toma": "fin",
+                "deja": "inicio"
+            }
+                },
+        "mano": {
+            "mensaje": "¡Has encontrado un tesoro! ¿Lo tomas o lo dejas?",
+            "opciones": {
+                "toma": "fin",
+                "deja": "inicio"
+            }
+        },
+    "Buenos Aires": {
+        "mensaje": "Bienvenido a la mision de Buenos Aires, ",
+        "opciones": {
+            "toma": "fin",
+            "deja": "inicio"
+        }
+    },
+            "fin": {
+                "mensaje": "El juego ha terminado. ¿Quieres jugar de nuevo?",
+                "opciones": {
+                    "si": "inicio",
+                    "no": None
+                }
+            }
+        }
+        self.estado_actual = "inicio"
+
+    def jugar(self):
+        while True:
+            decision = self.decisiones[self.estado_actual]
+            respuesta = input(decision["mensaje"] + " ").lower()  
+            for opcion, estado in decision["opciones"].items():
+                if opcion in respuesta:
+                    self.estado_actual = estado
+                    break
+            if not self.estado_actual:
+                break
+            
+nom = input("Ingresa tu nombre: ")
+print("Cargando...")
+time.sleep(2) 
+os.system('cls' if os.name == 'nt' else 'clear')
+juego = Juego(nom)
+juego.jugar()
