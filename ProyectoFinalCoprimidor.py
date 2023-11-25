72497a17c5a028adcd58af3a9ce8ad67cfc7d99a
RodrigoBG1 <129990916+RodrigoBG1@users.noreply.github.com>
2023-11-25 14:25:14 -0600

    Update ProyectoFinalCoprimidor.py

--- ProyectoFinalCoprimidor.py
+++ ProyectoFinalCoprimidor.py
@@ -4,6 +4,7 @@ from bitarray import bitarray
 from PIL import Image
 import librosa
 import numpy as np
+import os
 
 class Node:
     def __init__(self, simbolo = None, frecuencia = 0, hijo_izq = None, hijo_der = None):
@@ -57,7 +58,7 @@ class Text:
         nodo_actual = arbol_huffman
 
         for bit in bits:
-            if bit == '0':
+            if str(bit) == '0':
                 nodo_actual = nodo_actual.hijo_izq
             else:
                 nodo_actual = nodo_actual.hijo_der
@@ -66,82 +67,11 @@ class Text:
                 texto_recuperado += nodo_actual.simbolo
                 nodo_actual = arbol_huffman
 
-        with open("OneDrive\Documentos\Tercersemestre\Estructuras II\Parcial 3\Pruebas_txt\descomprimido.txt", 'w') as file:
+        with open("C:/Users/Rodrigo/OneDrive/Documentos/Compresor/descomprimido.txt", 'w') as file:
             file.write(texto_recuperado)
 
         return texto_recuperado
     
-class Image:
-    def obtener_imagen(self, direccion):
-        imagen = Image.open(direccion)
-        ancho, alto = imagen.size
-        return imagen, ancho, alto
-    
-    def frecuencia_simbolos_imagen(self, direccion):
-        frecuencia_pixeles = {}
-        imagen, a, b = self.obtener_imagen(direccion)
-
-        for pixel in imagen.getdata():
-            frecuencia_pixeles[pixel] = frecuencia_pixeles.get(pixel, 0) + 1
-
-        return frecuencia_pixeles
-    
-    def codificar_imagen(self, imagen, diccionario):
-        pixels = list(imagen.getdata())
-        imagen_codificada = ''.join(diccionario[pixel] for pixel in pixels)
-        return imagen_codificada
-    
-    def decodificar_imagen(self, bits, arbol_huffman, ancho, alto):
-        texto_recuperado = []
-        nodo_actual = arbol_huffman
-
-        for bit in bits:
-            if bit == '0':
-                nodo_actual = nodo_actual.hijo_izq
-            else:
-                nodo_actual = nodo_actual.hijo_der
-
-            if nodo_actual.simbolo is not None:
-                texto_recuperado.append(nodo_actual.simbolo)
-                nodo_actual = arbol_huffman
-
-        # Crea la imagen con el tamaño proporcionado
-        imagen_recuperada = Image.new('RGB', (ancho, alto))
-        imagen_recuperada.putdata(texto_recuperado)
-
-        imagen_recuperada.save("OneDrive\Documentos\Tercersemestre\Estructuras II\Parcial 3\imagen2_recuperada.jpg")
-
-        # Muestra la imagen usando el visor de imágenes predeterminado
-        imagen_recuperada.show()
-
-        return imagen_recuperada
-    
-class Audio:
-    """def audio_to_binary(self, file_path):
-        # Carga el archivo de audio
-        audio_data, sr = librosa.load(file_path)
-
-        # Cuantización (normalización a valores entre -1 y 1)
-        normalized_audio = audio_data / np.max(np.abs(audio_data))
-
-        # Muestreo (convierte a valores enteros entre -32768 y 32767, por ejemplo, para audio de 16 bits)
-        audio_int = np.int16(normalized_audio * 32767)
-
-        # Convierte los valores enteros a binario
-        binary_data = ''.join(format(sample, '016b') for sample in audio_int)
-        return binary_data
-    
-    def calcular_frecuencia_binaria(self, binary_data, longitud_simbolo=16):
-        frecuencia_binaria = {}
-        # Divide los datos binarios en segmentos de longitud_simbolo
-        simbolo = [binary_data[i:i+longitud_simbolo] for i in range(0, len(binary_data), longitud_simbolo)]
-
-        # Cuenta la frecuencia de cada símbolo
-        for simbolo in binary_data:
-            frecuencia_binaria[simbolo] = frecuencia_binaria.get(simbolo, 0) + 1
-        print(frecuencia_binaria)
-        return frecuencia_binaria""" 
-
 
 class Huffman:   
     def generar_arbol_huffman(self, frecuencia_letras):
@@ -190,7 +120,7 @@ class Huffman:
 Obj = Huffman()
 
 Obj1 = Text()
-Direccion = "OneDrive\Documentos\Tercersemestre\Estructuras II\Parcial 3\Pruebas_txt\input.txt"
+Direccion = "C:/Users/Rodrigo/OneDrive/Documentos/Compresor/input.txt"
 frecuencia_letras = Obj1.frecuencia_simbolos(Direccion)
 arbol_de_huffman = Obj.generar_arbol_huffman(frecuencia_letras)
 diccionario_codigos_nuevos = Obj.diccionario_codigos_nuevos(arbol_de_huffman)
@@ -199,8 +129,8 @@ text = Obj1.obtener_texto(Direccion)
 
 texto_codificado = Obj1.codificar_texto(text, diccionario_codigos_nuevos)
 
-archivo_comprimido = "OneDrive\Documentos\Tercersemestre\Estructuras II\Parcial 3\Pruebas_txt\o.bin"
-archivo_huff_comprimido = "OneDrive\Documentos\Tercersemestre\Estructuras II\Parcial 3\Pruebas_txt\salida_arbol.pkl"
+archivo_comprimido = "C:/Users/Rodrigo/OneDrive/Documentos/Compresor/o.bin"
+archivo_huff_comprimido = "C:/Users/Rodrigo/OneDrive/Documentos/Compresor/o.pkl"
 Obj1.comprimir(diccionario_codigos_nuevos, text, archivo_comprimido,arbol_de_huffman , archivo_huff_comprimido)
 Obj1.descomprimir(archivo_comprimido, archivo_huff_comprimido)
 
